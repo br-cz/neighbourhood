e4be1329674ddf8181741b9a9d5de9f233f2d0c5
Alborz Khakbazan <alvisk1313@gmail.com>
2024-02-09 18:23:08 -0600

    Metadata, Theme, Logo Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,9 @@ import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../theme';
 
 export const metadata = {
-  title: 'Mantine Next.js template',
-  description: 'I am using Mantine with Next.js!',
+  title: 'neighbourhood - your community at your fingertips',
+  description:
+    'Neighbourhood is a community platform that allows you to connect with your neighbours and local community. Its a place to spark discussion, host events, and build relationships through proximity.',
 };
 
 export default function RootLayout({ children }: { children: any }) {
@@ -17,6 +18,12 @@ export default function RootLayout({ children }: { children: any }) {
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap"
+          rel="stylesheet"
         />
       </head>
       <body>
