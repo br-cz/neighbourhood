d664f5eb062035aa27a0dee0d6d7de634a6353e2
Bricz <bricz.cruz@gmail.com>
2024-02-20 00:08:53 -0600

    Filter many to many user community table to get only the information regarding current community

--- src/api/memberQueries.ts
+++ src/api/memberQueries.ts
@@ -6,9 +6,28 @@ import { User } from '../models';
 const client = generateClient();
 
 export const useFetchMembers = () => {
-  const [events, setEvents] = useState<any>([]);
+  const [members, setMembers] = useState<any>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const ListUserCommunities = /* GraphQL */ `
+    query ListUserCommunities {
+      listUserCommunities {
+        nextToken
+        startedAt
+        items {
+          id
+          user {
+            id
+            username
+            profilePic
+          }
+          communityId
+          userId
+        }
+      }
+    }
+  `;
 
   useEffect(() => {
     const fetchMembers = async () => {
@@ -17,12 +36,16 @@ export const useFetchMembers = () => {
         const userData = localStorage.getItem('userData')!;
         const parsedUserData: User = JSON.parse(userData);
 
-        const community = await client.graphql({
-          query: getCommunity,
+        const members = await client.graphql({
+          query: ListUserCommunities,
           variables: { id: parsedUserData.selectedCommunity },
         });
 
-        setEvents(JSON.stringify(community.data.getCommunity?.members));
+        const jsonMembers = JSON.parse(JSON.stringify(members));
+        const filteredMembers = jsonMembers.data.listUserCommunities.items.filter(
+          (item: any) => item.communityId === parsedUserData.selectedCommunity
+        );
+        setMembers(filteredMembers);
       } catch (err: any) {
         setError(err);
       } finally {
@@ -33,5 +56,5 @@ export const useFetchMembers = () => {
     fetchMembers();
   }, []);
 
-  return { events, loading, error };
+  return { members, loading, error };
 };
