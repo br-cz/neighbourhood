7d3d0ce4b1854888000767fcba50d6c18489fb3d
Safran Bin Kader <118633138+sbinkader@users.noreply.github.com>
2024-02-27 21:14:52 -0600

    Added 1 more test + minor refactoring

--- __test__/editAccountDetails.test.tsx
+++ __test__/editAccountDetails.test.tsx
@@ -1,8 +1,17 @@
-import { render, waitFor } from '@testing-library/react';
+import { render, waitFor, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ProfilePage from '@/app/profile/page';
 import { MantineProvider } from '@mantine/core';
 import { DataProvider } from '@/contexts/DataContext';
+
+const renderComponent = () =>
+  render(
+    <MantineProvider>
+      <DataProvider>
+        <ProfilePage />
+      </DataProvider>
+    </MantineProvider>
+  );
 
 // Test cases for the ProfilePage component
 describe('ProfilePage - Change Password', () => {
@@ -11,18 +20,18 @@ describe('ProfilePage - Change Password', () => {
       configure: jest.fn(),
       updatePassword: jest.fn(() => Promise.resolve()),
       updateUserAttributes: jest.fn(() => Promise.resolve()),
-      // Add other methods you use and want to mock
     },
   }));
-  // Test case 1: Allows the user to change their password
+
+  // Test case 1: Renders the Account Detail Page
+  test('Renders Account Detail Page', async () => {
+    renderComponent();
+    expect(screen.getByText(/Edit Login Details/i)).toBeInTheDocument();
+  });
+
+  // Test case 2: Allows the user to change their password
   test('Allows the user to change their password', async () => {
-    const { getByLabelText, getByText } = render(
-      <MantineProvider>
-        <DataProvider>
-          <ProfilePage />
-        </DataProvider>
-      </MantineProvider>
-    );
+    renderComponent();
 
     require('@/src/hooks/usersCustomHooks').getCurrentUser.mockResolvedValue({
       id: 'userId',
@@ -30,9 +39,9 @@ describe('ProfilePage - Change Password', () => {
       _version: 1,
     });
 
-    await userEvent.type(getByLabelText(/Old Password/i), 'oldPassword');
-    await userEvent.type(getByLabelText(/New Password/i), 'newPassword');
-    await userEvent.click(getByText(/Submit Changes/i));
+    await userEvent.type(screen.getByLabelText(/Old Password/i), 'oldPassword');
+    await userEvent.type(screen.getByLabelText(/New Password/i), 'newPassword');
+    await userEvent.click(screen.getByText(/Submit Changes/i));
 
     await waitFor(() => {
       expect(require('@aws-amplify/auth').updatePassword).toHaveBeenCalledWith({
@@ -48,15 +57,9 @@ describe('ProfilePage - Change Password', () => {
     );
   });
 
-  // Test case 2: Allows the user to change their email
+  // Test case 3: Allows the user to change their email
   test('Allows the user to change their email', async () => {
-    const { getByLabelText, getByText } = render(
-      <MantineProvider>
-        <DataProvider>
-          <ProfilePage />
-        </DataProvider>
-      </MantineProvider>
-    );
+    renderComponent();
 
     require('@/src/hooks/usersCustomHooks').getCurrentUser.mockResolvedValue({
       id: 'userId',
@@ -71,8 +74,8 @@ describe('ProfilePage - Change Password', () => {
     updateUserAttributesMock.mockResolvedValue(true);
 
     // Simulate typing a new email
-    await userEvent.type(getByLabelText(/New Email/i), 'newuser@example.com');
-    await userEvent.click(getByText(/Submit Changes/i));
+    await userEvent.type(screen.getByLabelText(/New Email/i), 'newuser@example.com');
+    await userEvent.click(screen.getByText(/Submit Changes/i));
 
     await waitFor(() => {
       expect(updateUserAttributesMock).toHaveBeenCalledWith({
