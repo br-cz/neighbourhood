1803672cd3516d45f45c7d5c486471242598f0f8
Safran Bin Kader <118633138+sbinkader@users.noreply.github.com>
2024-03-01 22:59:16 -0600

    More Enhancements to Community Card + Add More Button

--- components/CommunityCard/CommunityCard.tsx
+++ components/CommunityCard/CommunityCard.tsx
@@ -1,18 +1,5 @@
 import React from 'react';
-import {
-  Avatar,
-  Text,
-  Group,
-  Badge,
-  Card,
-  Stack,
-  SimpleGrid,
-  Button,
-  Space,
-  Flex,
-  Grid,
-  rem,
-} from '@mantine/core';
+import { Avatar, Text, Card, Stack, Button, Flex, Grid } from '@mantine/core';
 
 interface CommunityCardProps {
   name: string;
@@ -24,28 +11,36 @@ interface CommunityCardProps {
 export default function CommunityCard({ name, image, members, friends }: CommunityCardProps) {
   return (
     <Stack align="center">
-      <Card shadow="md" padding="md" radius="md" withBorder w="50vw">
-        <Grid align="flex-end">
-          <Grid.Col span={3}>
-            <Avatar src={image} size="10vw" />
-          </Grid.Col>
-          <Grid.Col span={4} style={{ minHeight: rem(130) }}>
-            <Text size="xl" fw={600}>
-              {name}
-            </Text>
-            <Text c="dimmed" size="md">
-              Members: {members.length}
-            </Text>
-            <Text c="dimmed" size="md">
-              Friends: {friends.length}
-            </Text>
+      <Card shadow="md" withBorder w="27vw">
+        <Grid align="stretch">
+          <Grid.Col span={2}>
+            <Avatar src={image} size="3.5vw" mt="xl" mb="xl" />
           </Grid.Col>
           <Grid.Col span={5}>
-            <Flex gap="md" direction="row" justify="end">
-              <Button size="sm" variant="filled">
+            <Flex direction="column" justify="center" style={{ height: '100%' }}>
+              <Text size="xl" fw={600}>
+                {name}
+              </Text>
+              <Text c="dimmed" size="md">
+                Members: {members.length}
+              </Text>
+              <Text c="dimmed" size="md">
+                Friends: {friends.length}
+              </Text>
+            </Flex>
+          </Grid.Col>
+          <Grid.Col span={5}>
+            <Flex
+              gap="xl"
+              direction="row"
+              justify="flex-end"
+              align="flex-end"
+              style={{ height: '100%' }}
+            >
+              <Button size="sm" variant="outline">
                 Select
               </Button>
-              <Button size="sm" variant="filled">
+              <Button size="sm" variant="filled" bg="red">
                 Leave
               </Button>
             </Flex>
@@ -54,64 +49,4 @@ export default function CommunityCard({ name, image, members, friends }: Communi
       </Card>
     </Stack>
   );
-}
-
-{
-  /* <Card shadow="sm" padding="sm" radius="md" withBorder w="35vw">
-      <Avatar src={image} size="100" />
-      {/* <Flex justify="center" gap="xs" direction="column" align="center"> }
-      <Text size="lg" fw={600}>
-        {name}
-      </Text>
-      <Text c="dimmed" size="md">
-        Members: {members.length}
-      </Text>
-      <Text c="dimmed" size="md">
-        Friends: {friends.length}
-      </Text>
-      {/* </Flex> }
-      <Flex justify="flex-end" gap="xl">
-        <Button size="md" variant="filled">
-          Select
-        </Button>
-        <Button size="md" variant="filled">
-          Leave
-        </Button>
-      </Flex>
-    </Card> */
-}
-
-{
-  /* <Card shadow="md" padding="xs" radius="md" withBorder w="55vw">
-<Grid align="center">
-  <Grid.Col span={3}>
-    <Avatar src={image} size="130" />
-  </Grid.Col>
-  {/* <Flex justify="center" gap="xs" direction="column" align="center"> }
-  <Grid.Col span={5}>
-    {/* <Flex justify="center"> }
-    <Text size="xl" fw={600}>
-      {name}
-    </Text>
-    <Text c="dimmed" size="md">
-      Members: {members.length}
-    </Text>
-    <Text c="dimmed" size="md">
-      Friends: {friends.length}
-    </Text>
-    {/* </Flex> }
-  </Grid.Col>
-  {/* </Flex> }
-  <Grid.Col span={4}>
-    <Flex gap="xl" direction="row" justify="flex-end">
-      <Button size="sm" variant="filled">
-        Select
-      </Button>
-      <Button size="sm" variant="filled">
-        Leave
-      </Button>
-    </Flex>
-  </Grid.Col>
-</Grid>
-</Card> */
 }
