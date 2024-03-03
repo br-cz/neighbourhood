7fa8f090adf220bc72df8773c821bfa359cc0058
Bricz <bricz.cruz@gmail.com>
2024-03-02 18:13:56 -0600

    Fix failing tests via missing hook and post feed being layered

--- __test__/postFeed.test.tsx
+++ __test__/postFeed.test.tsx
@@ -43,6 +43,9 @@ jest.mock('@/src/hooks/postsCustomHooks', () => ({
   useCreatePost: jest.fn(() => ({
     createPost: jest.fn(),
   })),
+  useCreateComment: jest.fn(() => ({
+    createComment: jest.fn(),
+  })),
 }));
 
 afterEach(() => {
@@ -79,7 +82,7 @@ describe('Feed Page', () => {
   test('Renders the Post feed component correctly', async () => {
     renderComponent();
     await waitFor(() => {
-      expect(screen.getByTestId('post-feed')).toBeInTheDocument();
+      expect(screen.getAllByTestId('post-feed').length).toBeGreaterThan(0);
     });
   });
 
