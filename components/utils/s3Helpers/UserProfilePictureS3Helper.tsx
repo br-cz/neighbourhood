7d6c64771a525d245dc072517aa364f6987f13b6
“GurmanToor” <gtoor243@gmail.com>
2024-03-03 20:08:23 -0600

    fix typo in userprofilepic helper

--- components/utils/s3Helpers/UserProfilePictureS3Helper.tsx
+++ components/utils/s3Helpers/UserProfilePictureS3Helper.tsx
@@ -1,9 +1,9 @@
-import { generateClient } from '@aws-amplify/api';
-import { uploadData, getUrl } from '@aws-amplify/storage';
+import { generateClient } from 'aws-amplify/api';
+import { uploadData, getUrl } from 'aws-amplify/storage';
 import { updateUser } from '@/src/graphql/mutations';
 
 import ConfigureAmplifyClientSide from '@/components/ConfigureAmplify';
-import { getUserAPI, updateUserProfilePicAPI } from '@/src/api/services/user';
+import { getUserAPI } from '@/src/api/services/user';
 
 ConfigureAmplifyClientSide();
 const client = generateClient();
@@ -28,9 +28,15 @@ export async function storeImage(file: File, userId: string) {
         data: file,
       }).result;
 
-      console.log(uploadResult.key);
-
-      await updateUserProfilePicAPI(userId, uploadResult.key, user._version);
+      await client.graphql({
+        query: updateUser,
+        variables: {
+          input: {
+            id: user.id,
+            profilePic: uploadResult.key,
+          },
+        },
+      });
 
       return uploadResult.key;
     }
@@ -44,10 +50,7 @@ export async function retrieveImage(userId: string) {
   const user = await getUserAPI(userId);
   if (user) {
   const imageKey = user.profilePic;
-  console.log(`imageKey: ${imageKey}`);
-  console.log(`contains id: ${imageKey?.includes(userId)}`);
   if (imageKey) {
-    if (imageKey)
     try {
        const result = await getUrl({
         key: imageKey,
