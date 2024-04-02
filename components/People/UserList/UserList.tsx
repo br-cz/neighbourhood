1c6af15adc01f967384df237d8958e53acb4e87b
Safran Bin Kader <118633138+sbinkader@users.noreply.github.com>
2024-04-02 05:48:07 -0500

    Temporary Removal of Tabs

--- components/People/UserList/UserList.tsx
+++ components/People/UserList/UserList.tsx
@@ -71,106 +71,51 @@ export function UserList({ searchQuery, sortQuery }: UserListProps) {
   ]);
 
   return (
-    <Tabs variant="outline" radius="md" defaultValue="all" data-testid="user-list">
-      <Tabs.List>
-        <Tabs.Tab value="all">All</Tabs.Tab>
-        <Tabs.Tab value="friends">Friends</Tabs.Tab>
-        {numRequests > 0 ? (
-          <Indicator size="md" inline label={numRequests} color="blue">
-            <Tabs.Tab value="requests">Requests</Tabs.Tab>
-          </Indicator>
-        ) : (
-          <Tabs.Tab value="requests">Requests</Tabs.Tab>
-        )}
-      </Tabs.List>
-      <Tabs.Panel value="all">
-        {loading ? (
-          <Group justify="center" mt="200">
-            <Loader />
-          </Group>
-        ) : incomingRequests.length === 0 &&
-          friends.length === 0 &&
-          outgoingRequests.length === 0 &&
-          noRelationshipFriends.length === 0 ? (
-          <Group justify="center" mt="200">
-            <Text c="dimmed">No users found</Text>
-          </Group>
-        ) : (
-          <SimpleGrid cols={1} spacing="xs" mt="sm" className={classes.list}>
-            {incomingRequests.map((user: any) => (
-              //Refactor status into Enum
+    <>
+      {loading ? (
+        <Group justify="center" mt="200">
+          <Loader />
+        </Group>
+      ) : incomingRequests.length === 0 &&
+        friends.length === 0 &&
+        outgoingRequests.length === 0 &&
+        noRelationshipFriends.length === 0 ? (
+        <Group justify="center" mt="200">
+          <Text c="dimmed">No users found</Text>
+        </Group>
+      ) : (
+        <SimpleGrid cols={1} spacing="xs" mt="sm" className={classes.list}>
+          {incomingRequests.map((user: any) => (
+            //Refactor status into Enum
+            <UserListItem
+              key={user.id}
+              user={user}
+              relationshipStatus="incoming"
+              onUpdate={refetch}
+            />
+          ))}
+          {friends &&
+            friends.map((user: any) => (
               <UserListItem
                 key={user.id}
                 user={user}
-                relationshipStatus="incoming"
+                relationshipStatus="friend"
                 onUpdate={refetch}
               />
             ))}
-            {friends &&
-              friends.map((user: any) => (
-                <UserListItem
-                  key={user.id}
-                  user={user}
-                  relationshipStatus="friend"
-                  onUpdate={refetch}
-                />
-              ))}
-            {outgoingRequests.map((user: any) => (
-              <UserListItem
-                key={user.id}
-                user={user}
-                relationshipStatus="outgoing"
-                onUpdate={refetch}
-              />
-            ))}
-            {noRelationshipFriends.map((user: any) => (
-              <UserListItem
-                key={user.id}
-                user={user}
-                relationshipStatus="none"
-                onUpdate={refetch}
-              />
-            ))}
-          </SimpleGrid>
-        )}
-      </Tabs.Panel>
-      <Tabs.Panel value="friends">
-        {friends.length === 0 ? (
-          <Group justify="center" mt="200">
-            <Text c="dimmed">No friends found</Text>
-          </Group>
-        ) : (
-          <SimpleGrid cols={1} spacing="xs" mt="sm" className={classes.list}>
-            {friends &&
-              friends.map((user: any) => (
-                <UserListItem
-                  key={user.id}
-                  user={user}
-                  relationshipStatus="friend"
-                  onUpdate={refetch}
-                />
-              ))}
-          </SimpleGrid>
-        )}
-      </Tabs.Panel>
-      <Tabs.Panel value="requests">
-        {numRequests === 0 ? (
-          <Group justify="center" mt="200">
-            <Text c="dimmed">No requests found</Text>
-          </Group>
-        ) : (
-          <SimpleGrid cols={1} spacing="xs" mt="sm" className={classes.list}>
-            {incomingRequests.map((user: any) => (
-              <UserListItem
-                key={user.id}
-                user={user}
-                relationshipStatus="incoming"
-                onUpdate={refetch}
-              />
-            ))}
-          </SimpleGrid>
-        )}
-      </Tabs.Panel>
-    </Tabs>
+          {outgoingRequests.map((user: any) => (
+            <UserListItem
+              key={user.id}
+              user={user}
+              relationshipStatus="outgoing"
+              onUpdate={refetch}
+            />
+          ))}
+          {noRelationshipFriends.map((user: any) => (
+            <UserListItem key={user.id} user={user} relationshipStatus="none" onUpdate={refetch} />
+          ))}
+        </SimpleGrid>
+      )}
+    </>
   );
 }
