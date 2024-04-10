b4d480d1e150a2145c304b440ae4c166bcaeb56b
Bricz <bricz.cruz@gmail.com>
2024-04-09 22:49:59 -0500

    Add utils mapper to jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,6 +11,7 @@ const customJestConfig = {
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
     '^@/src/hooks/(.*)$': '<rootDir>/src/hooks/$1',
     '^@/src/api/services/(.*)$': '<rootDir>/src/api/services/$1',
+    '^@/src/utils/(.*)$': '<rootDir>/src/utils/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 };
